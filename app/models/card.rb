fc7891f249b14573878409307e9b05dd533ed232
Katie Delfin <katiedelfin@gmail.com>
2015-10-10 09:18:52 -0400

    Create for generating new cards

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,16 @@
 class Card < ActiveRecord::Base
   validates :fun_fact_ids, uniqueness: true, length: { is: 50 }, card_integrity: true
 
+  def self.generate
+    users = User.includes(:fun_facts).order("RANDOM()").limit(50)
+    random_fun_fact_ids = users.map do |user|
+      user.fun_facts.shuffle[0].id
+    end
+
+    self.create(fun_fact_ids: random_fun_fact_ids)
+  end
+
   def fun_facts
     FunFact.where(id: fun_fact_ids)
   end
-end 
+end
