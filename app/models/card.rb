46abf44d3e48ea7d57e2ac0f0cb9cf385e764ebf
Katie Delfin <katiedelfin@gmail.com>
2015-10-14 14:08:36 -0400

    Use 48 sized card

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,8 +1,10 @@
 class Card < ActiveRecord::Base
-  validates :fun_fact_ids, uniqueness: true, length: { is: 50 }, card_integrity: true
+  CARD_SIZE = 48
+
+  validates :fun_fact_ids, uniqueness: true, length: { is: CARD_SIZE }, card_integrity: true
 
   def self.generate
-    users = User.includes(:fun_facts).order("RANDOM()").limit(50)
+    users = User.includes(:fun_facts).order("RANDOM()").limit(CARD_SIZE)
     random_fun_fact_ids = users.map do |user|
       user.fun_facts.shuffle[0].id
     end
